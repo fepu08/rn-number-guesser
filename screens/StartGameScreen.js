1d351e4f3b1552508582b54a6efdc9499f7b16d3
fepu08 <ferencbesenyei@protonmail.com>
2025-12-15 19:13:04 +0100

    SHADOW styling props

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,18 +1,45 @@
-import {StyleSheet, View, TextInput} from "react-native";
+import { StyleSheet, View, TextInput } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 const StartGameScreen = () => {
-    return(
-        <View>
-            <TextInput/>
-            <PrimaryButton>Reset</PrimaryButton>
-            <PrimaryButton>Confirm</PrimaryButton>
-        </View>
-    )
-}
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
 
-})
+    // android only shadow
+    // elevation: 4,
 
-export default StartGameScreen
+    // iOS shadow styles
+    // shadowColor: "black",
+    // shadowOffset: { width: 0, height: 2 },
+    // shadowRadius: 6,
+    // shadowOpacity: 0.25,
+
+    // For newer Architectures
+    boxShadow: [
+      { offsetX: 0, offsetY: 4, blurRadius: 10, color: "rgba(0,0,0,0.8)" },
+      {
+        inset: true,
+        offsetX: 0,
+        offsetY: 2,
+        blurRadius: 5,
+        color: "rgba(255,0,0,0.1)",
+      },
+    ],
+  },
+});
+
+export default StartGameScreen;
