e8a99cc007ffc4cd35a58c77cb7926aa504b6aae
fepu08 <ferencbesenyei@protonmail.com>
2025-12-18 20:34:45 +0100

    added gradient background color and background image

--- App.js
+++ App.js
@@ -1,15 +1,32 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, ImageBackground } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={styles.container}>
-        <StartGameScreen />
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <LinearGradient style={styles.container} colors={["#4e0329", "#ddb52f"]}>
+      <ImageBackground
+        source={require("./assets/images/background.png")}
+        resizeMode={"cover"}
+        style={styles.container}
+        imageStyle={styles.imageBackground}
+      >
+        <SafeAreaProvider>
+          <SafeAreaView style={styles.container}>
+            <StartGameScreen />
+          </SafeAreaView>
+        </SafeAreaProvider>
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  imageBackground: {
+    opacity: 0.15,
+  },
+});
