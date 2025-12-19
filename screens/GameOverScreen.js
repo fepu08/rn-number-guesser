3735837810f4da39807af0836d5259910a8c96a9
fepu08 <ferencbesenyei@protonmail.com>
2025-12-19 21:47:41 +0100

    updating game over screen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,7 +1,8 @@
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, Image, ImageBackground } from "react-native";
 import Title from "../components/ui/Title";
 import PrimaryButton from "../components/ui/PrimaryButton";
 import { useGameContext } from "../components/context/gameContext";
+import Colors from "../constants/colors";
 
 const GameOverScreen = () => {
   const {
@@ -12,18 +13,19 @@ const GameOverScreen = () => {
   return (
     <View style={styles.container}>
       <Title>Game Over</Title>
-      <View style={styles.resultTextContainer}>
-        <Text style={styles.resultText}>Your number is: </Text>
-        <Text style={[styles.resultText, styles.resultNumber]}>
-          {guessNumber}
-        </Text>
+      <View style={styles.imageContainer}>
+        <ImageBackground
+          source={require("../assets/images/success.png")}
+          style={styles.image}
+        />
       </View>
-      <View>
-        <PrimaryButton onPress={resetGame} textStyles={styles.button}>
-          Play Again
-        </PrimaryButton>
-      </View>
-      <View></View>
+      <Text style={styles.summaryText}>
+        Your phone needed <Text style={styles.highlight}>X</Text> rounds to
+        guess the number <Text style={styles.highlight}>Y</Text>.
+      </Text>
+      <PrimaryButton onPress={resetGame} textStyles={styles.button}>
+        Play Again
+      </PrimaryButton>
     </View>
   );
 };
@@ -31,24 +33,37 @@ const GameOverScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "space-between",
-    width: "50%",
-    marginHorizontal: "auto",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imageContainer: {
+    borderRadius: 150,
+    width: 300,
+    height: 300,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    marginVertical: 36,
+    overflow: "hidden",
+  },
+  image: {
+    flex: 1,
+    //width: "100%",
+    //height: "100%",
+  },
+  summaryText: {
+    fontFamily: "open-sans",
+    fontSize: 18,
+    textAlign: "center",
+    marginBottom: 36,
+  },
+  highlight: {
+    fontFamily: "open-sans-bold",
+    color: Colors.primary500,
   },
   button: {
     fontSize: 24,
     paddingVertical: 12,
     paddingHorizontal: 24,
-  },
-  resultTextContainer: {
-    flexDirection: "row",
-  },
-  resultText: {
-    color: "white",
-    fontSize: 24,
-  },
-  resultNumber: {
-    fontWeight: "bold",
   },
 });
 
